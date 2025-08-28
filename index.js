fd73c28af5ed91abe578b878f3dd69d4b39f7886
unknown <mahedishowrov@gmail.com>
2025-08-28 14:14:51 +0600

    work for copy button and call history with js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const lovesCount = document.getElementById('love-count')
 const loves = document.querySelectorAll('.loves');
 
 let count = 0;
-loves.forEach(function (love) {
+for (let love of loves) {
     love.addEventListener('click', function () {
         count = count + 1;
         lovesCount.innerText = count;
     });
-});
+};
 // Call button js....
 const cardButtons = document.getElementsByClassName('card-button');
 console.log(cardButtons)
 
 for (let cardButton of cardButtons) {
     cardButton.addEventListener("click", function () {
-        const servesTital = cardButton.parentNode.parentNode.childNodes[3];
-        const servesNumber = cardButton.parentNode.parentNode.childNodes[7];
+        const servesTital = cardButton.parentNode.parentNode.querySelector("h1").innerText;
+        const servesNumber = cardButton.parentNode.parentNode.querySelector("h2").innerText;
         let presentDimond = parseInt(document.getElementById('daimond').innerText);
 
         if (presentDimond < 20) {
@@ -31,8 +31,51 @@ for (let cardButton of cardButtons) {
             document.getElementById('daimond').innerText = newDimond;
         }
 
-        alert("...calling..." + servesTital.innerText + " " + servesNumber.innerText);
+        alert("...calling..." + servesTital + " " + servesNumber);
 
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        const hour12 = hours % 12 || 12;
+        const currentTime = `${hour12}:${minutes}:${seconds} ${ampm}`;
+        const asideContainer = document.getElementById('aside-container')
+        const newCard = document.createElement('div');
+        newCard.innerHTML = `
+        <div
+            class="flex justify-between items-center bg-slate-50 rounded-lg p-4"
+          >
+            <div>
+              <h1>${servesTital}</h1>
+              <p>${servesNumber}</p>
+            </div>
+            <p>${currentTime}</p>
+          </div>`;
+
+        asideContainer.append(newCard);
 
     })
 }
+
+// copy button....
+const CopyCount = document.getElementById('Copy-Count')
+const CopyBtns = document.querySelectorAll('.Copy-btn');
+
+let sum = 0;
+for (let CopyBtn of CopyBtns) {
+    CopyBtn.addEventListener('click', async function () {
+        const servesNumber = CopyBtn.parentNode.parentNode.querySelector("h2").innerText;
+
+        sum = sum + 1;
+        CopyCount.innerText = sum;
+        alert("Number copied: " + servesNumber)
+
+
+        await navigator.clipboard.writeText(servesNumber)
+
+
+
+    });
+
+};
